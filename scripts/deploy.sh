cefc83b627b13f4855b89ca60357ff8034324cb9
Søren Holm <sgh@sgh.dk>
2014-03-01 00:41:14 +0100

    Added deplay-script.

---
+++ scripts/deploy.sh
@@ -0,0 +1,8 @@
+VERSION=`git describe --exact-match HEAD^1`
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+DESTFILE="FOE_KlanManager_${VERSION}.exe"
+echo Deplaying ${VERSION}
+mv install.exe ${DESTFILE}
+scp ${DESTFILE} sgh.dk:public_html/
