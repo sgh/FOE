d4391138cc75a6eba386388f73b1c75c86527ea7
Søren Holm <sgh@sgh.dk>
2015-04-11 00:01:50 +0200

    Remove clanname from user edit.

--- src/foeusereditdlg.cpp
+++ src/foeusereditdlg.cpp
@@ -89,7 +89,7 @@ FoeUserEditDlg::FoeUserEditDlg(FoeUser *user, QWidget *parent) :
 	ui(new Ui::FoeUserEditDlg)
 {
 	ui->setupUi(this);
-	ui->username_label->setText(QString("%1 - %2").arg(_user->name()).arg(_user->clanName()));
+	ui->username_label->setText(QString("%1").arg(_user->name()));
 	setWindowTitle(windowTitle().arg(_user->name()).arg(_user->clanName()));
 
 	ui->toolBox->setCurrentIndex(0);
